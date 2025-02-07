38dbfe46e34adf6863bcae194421b9e8e419994a
Joshua Ely <joshy.ely@outlook.com>
2025-02-07 14:52:49 +0000

    Made changes to the bash script to make it more convenient to run the frontend server inside or outside the directory

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,9 +1,16 @@
 #! /usr/bin/bash
+run() {
+    npm install
+    npm run dev -- --port 3000
+}
+
 
 if [[ ! $PWD =~ "frontend" ]]
 then
     cd "$PWD/frontend"
+    run
+    cd ..
+else
+    run
 fi
 
-npm install
-npm run dev -- --port 3000
