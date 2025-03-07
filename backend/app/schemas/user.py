bc65c07d79d9e4b4d00817849066fedfda3eb2c2
Joshua Ely <joshy.ely@outlook.com>
2025-03-07 17:25:58 +0000

    Added field variables for names and password, and added validator for password on registration

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,9 +1,12 @@
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field, EmailStr, field_validator
+import re 
 from typing import Annotated, Optional
 from datetime import date
 
 TEXT_FIELD = Annotated[str, Field(max_length=50)]
+NAME_FIELD = Annotated[str, Field(max_length=50, pattern='^[a-zA-Z]+$')]
 EMAIL_FIELD = Annotated[EmailStr, Field(max_length=50)]
+PASSWORD_FIELD = Annotated[str, Field(min_length=8, max_length=50)]
 DOB_FIELD = Optional[date]
 PROMOTIONS_FIELD = Annotated[bool, Field(default=False)]
 
@@ -14,9 +17,23 @@ class UserLogin(UserBase):
     password: TEXT_FIELD
 
 class UserRegister(UserBase):
-    password: TEXT_FIELD
-    first_name: TEXT_FIELD
-    last_name: TEXT_FIELD
+    password: PASSWORD_FIELD
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value:str) -> str:
+        """Custom password validation to ensure it meets security criteria."""
+        if not re.search(r"[a-z]", value):
+            raise ValueError("Password must contain at least one lowercase letter.")
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("Password must contain at least one uppercase letter.")
+        if not re.search(r"[0-9]", value):
+            raise ValueError("Password must contain at least one number.")
+        if not re.search(r"[@&£$%^&()\-]", value):
+            raise ValueError("Password must contain at least one special character (@&£$%^&()-).")
+        return value
+    
+    first_name: NAME_FIELD
+    last_name: NAME_FIELD
     dob: DOB_FIELD
     receive_promotions: PROMOTIONS_FIELD
 
